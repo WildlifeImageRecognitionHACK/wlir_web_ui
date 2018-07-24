dc5d69a0d344c89520e6e1d770e6c3e4b0315f41
Jeremy Kayes <jekayes@microsoft.com>
2018-07-24 14:00:28 -0700

    Applying API check.

--- src/js/wlir.js
+++ src/js/wlir.js
@@ -14,6 +14,18 @@ wlirApp.config(function($routeProvider,$locationProvider,$scope) {
 wlirApp.controller("wlirInit", ["$scope","$rootScope","$http","$location","$route", 
   function($scope,$rootScope,$http,$location,$route) {
 
-  $scope.login = "Please Login";
+    $scope.login = "Please Login";
+    var api_root = ("//" + config.api.url + ":" + config.api.port + "/");
+
+
+    $http({
+      method: "GET",
+      url: api_root
+    }).then(function successCallback(response) {
+      $scope.api_connect = "We're connected!";
+    }, function errorCallback(response) {
+      $scope.api_connect = "We couldn't connect!";
+      console.log("API Error!");
+    });
 
 }]);
